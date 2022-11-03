084edbf365d4d446696afff12f12babf0b99158e
jacob-gg <jrg2xq@virginia.edu>
2022-11-03 16:04:14 -0400

    date update

--- non_person_regex.R
+++ non_person_regex.R
@@ -2,7 +2,7 @@
 # Regex pattern for identifying non-person names #
 # Jacob Goldstein-Greenwood                      #
 # GitHub: jacob-gg                               #
-# Last revised: 2022-11-01                       #
+# Last revised: 2022-11-03                       #
 ##################################################
 
 non_person_regex <- paste0('(?i)',
